f4cb16547b8c1e14a5bebb143ffd441985289d18
vexgrave <pavkrp2021@gmail.com>
2026-06-01 02:43:22 +0300

    Лабораторная работа 3

--- src/db/tui.py
+++ src/db/tui.py
@@ -63,7 +63,7 @@ class DatabaseTUI:
     def add_record_menu(self):
         tables = self.db.list_tables()
         if not tables:
-            print("Нет доступных таблиц. Cначала создайте таблицу.")
+            print("Нет доступных таблиц.")
             return
         print(f"Доступные таблицы: {tables}")
         tbl_name = input("Введите название таблицы: ").strip()
